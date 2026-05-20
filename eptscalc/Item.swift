c8723de179407a6eaf19ba2db0f3a4887f5aed95
Marc <mmelcher@gmail.com>
2026-05-20 14:12:38 -0700

    Initial Commit

---
+++ eptscalc/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  eptscalc
+//
+//  Created by Marc L. Melcher on 5/20/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
